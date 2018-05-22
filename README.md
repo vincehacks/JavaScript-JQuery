2d54d433ef1c05026986a7d52b885bd562d8e374
Vincent Chang <vincent.chang@macys.com>
2018-05-21 22:55:37 -0700

    Added JS practice

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # JavaScript-JQuery
+Created by Vince Chang </br>
+
 Practicing basic functionality of JS and JQuery
--- jsPractice.js
+++ jsPractice.js
@@ -1,7 +1,7 @@
 // Created by Vince Chang
 // Run this file: node jsPractice.js
 
-// Example of a basic Person functionality done by a function
+// Person functionality done by a function
 function Person1(name, friends){
   this.name = name;
   this.friends = friends;
@@ -48,17 +48,21 @@ console.log("\n");
 /******************************************************************************/
 
 // FOR OF LOOPS
-console.log("FUNCTION: forOfLoop");
+console.log("FUNCTION: forOfLoop()");
 function forOfLoop(){
   var array = [1,2,3];
+  console.log('Printing the elements in [1,2,3] using a for of loop');
   for(let element of array){
     console.log(element);
   }
 } forOfLoop();
+console.log("\n");
+/******************************************************************************/
 
-// Practicing how to use a default value, 3 ways to do this:
+// Using a default value, 3 ways to do this:
 // Pass in a default value, use ternary operator, or the || operator
-var multBy2 = function(value){
+console.log("FUNCTION: multBy2()");
+var multBy2 = function(value = 2){
   // Ternary
   // var defaultVal = (value)?value:0;
 
@@ -68,103 +72,93 @@ var multBy2 = function(value){
   // return defaultVal * 2;
   return value *2;
 }
+console.log('2 * 2 = ' + multBy2());
+console.log("\n");
+/******************************************************************************/
 
-console.log(multBy2());
+// Using Arguments
+console.log("FUNCTION: sum()");
+function sum(){
+  var sum = 0; // arguments.length will be the number of arguments passed in
+  for(var i = 0; i < arguments.length; i++){
+    sum += arguments[i];
+  }
+  return sum;
+}
 
-// // Using Arguments
-// function sum(){
-//   var sum = 0;
-//   for(var i = 0; i < arguments.length; i++){
-//     sum += arguments[i];
-//   }
-//   return sum;
-// }
-
-// console.log(sum(1,2,3,4,5));
-
-
+console.log('1 + 2 + 3 + 4 + 5 = ' + sum(1,2,3,4,5)); // Results to 15
+console.log("\n");
+/******************************************************************************/
 
 // ... Spread operator
-// function sum(...arrayParam){
-//   var sum = 0;
-//   for(var i = 0; i < arrayParam.length; i++){
-//     sum += arrayParam[i];
-//   }
+console.log("FUNCTION: spreadSum()");
+function spreadSum(...arrayParam){
+  var sum = 0;
+  for(var i = 0; i < arrayParam.length; i++){
+    sum += arrayParam[i];
+  }
 
-//   return sum;
-// }
+  return sum;
+}
 
-// const values = [1,2,3,4,5];
-// console.log(sum(...values));
+const values = [1,2,3,4,5];
+console.log('1 + 2 + 3 + 4 + 5 = ' + spreadSum(...values)); // Results to 15
+console.log("\n");
+/******************************************************************************/
 
+// New lines without using the + operator, just need to use backticks ``
+// ${} inserts JS into a string
+function newNewLine(){
+  console.log("FUNCTION: newNewLine()");
+  var adjective = 'awesome';
+  var template =`  I wrote a haiku
+  This is so very ${adjective}
+  Now I am finished`;
 
+  console.log(template);
+} newNewLine();
+console.log("\n");
+/******************************************************************************/
 
-// New lines without using the +
-// var adjective = 'awesome';
-// var template = `I wrote a haiku
-// This is so very ${adjective}
-// Now I am finished`;
-
-// console.log(template);
-
-
-// // Trying to use inline JS in a string using ${}
-// var name = "Vince";
-// function yell(value){
-//     return value.toUpperCase();
-// }
-// console.log (`Hello ${yell(name)}!`);
-
+// Inline JS in a string using ${}
+var name = "Vince";
+console.log("FUNCTION: yell()");
+function yell(value){
+  return value.toUpperCase();
+}
+console.log (`Hello ${yell(name)}!`);
+console.log("\n");
+/******************************************************************************/
 
 // Object Literal Upgrades
-// var a = 8;
-// var b = 9;
-// var c = 10;
+function objLiteralUpgrades(){
+  console.log("FUNCTION: objLiteralUpgrades()");
 
-// var obj = {
-//   // this is like a = a; etc
-//   a,
-//   b,
-//   c,
-//   d(){
-//       console.log(this.a, this.b, this.c);
-//   }
-// }
-// c = 100; // this won't change c, because `c,` is just a copy!
-// obj.d();
+  var a = 8;
+  var b = 9;
+  var c = 10;
 
+  var obj = { // This is saying a = a => a = 8
+    a,
+    b,
+    c,
+    d(){
+      console.log('a = ' + this.a + ' b = ' + this.b + ' c = ' + this.c);
+    }
+  }
+  c = 100; // This won't change c, because `c,` is just a copy!
+  obj.d(); // d is still 8, 9 , 10
+  console.log("\n");
+} objLiteralUpgrades();
+/******************************************************************************/
 
-// Array Destruction
-// var a, b;
-// [a,,b] = [1,2,3,4];
-// console.log(a); // a = 1
-// console.log(b); // b = 3
+// Array Destruction, this is one to one mapping
+function arrayDestruction(){
+  console.log("FUNCTION: arrayDestruction()");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  var a, b;
+  [a,,b] = [1,2,3,4];
+  console.log('a is ' + a); // a = 1
+  console.log('b is ' + b); // b = 3
+  console.log("\n");
+} arrayDestruction();
